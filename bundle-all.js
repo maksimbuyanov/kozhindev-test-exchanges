f7503a6398294aa32a0439785014a6b660de3b8b
maksimbuyanov <maksi_777@bk.ru>
2022-05-12 22:36:51 +0700

    Updates

---
+++ bundle-all.js
@@ -0,0 +1,3 @@
+const link = document.createElement('link'); link.rel = 'stylesheet', link.href = './frontend/bundle-index.bd2889285f9f5c171f96.css', document.getElementsByTagName('head')[0].appendChild(link);
+const script0 = document.createElement('script'); script0.src = './frontend/bundle-common.bd2889285f9f5c171f96.js', script0.type = 'text/javascript', script0.async = true, document.getElementsByTagName('body')[0].appendChild(script0);
+const script2 = document.createElement('script'); script2.src = './frontend/bundle-index.bd2889285f9f5c171f96.js', script2.type = 'text/javascript', script2.async = true, document.getElementsByTagName('body')[0].appendChild(script2);
