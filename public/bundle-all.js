73f1ec5822d56496ef90d3a90f7dbd457dec7f15
maksimbuyanov <maksi_777@bk.ru>
2022-05-12 18:32:00 +0700

    build project

--- public/bundle-all.js
+++ public/bundle-all.js
@@ -1,3 +1,3 @@
-var link=document.createElement("link");link.rel="stylesheet",link.href="/frontend/bundle-index.e066c3e019fea624334c.css",document.getElementsByTagName("head")[0].appendChild(link);
-var script0=document.createElement("script");script0.src="/frontend/bundle-common.e066c3e019fea624334c.js",script0.type="text/javascript",script0.async=true,document.getElementsByTagName("body")[0].appendChild(script0);
-var script2=document.createElement("script");script2.src="/frontend/bundle-index.e066c3e019fea624334c.js",script2.type="text/javascript",script2.async=true,document.getElementsByTagName("body")[0].appendChild(script2);
+var link=document.createElement("link");link.rel="stylesheet",link.href="/frontend/bundle-index.bd2889285f9f5c171f96.css",document.getElementsByTagName("head")[0].appendChild(link);
+var script0=document.createElement("script");script0.src="/frontend/bundle-common.bd2889285f9f5c171f96.js",script0.type="text/javascript",script0.async=true,document.getElementsByTagName("body")[0].appendChild(script0);
+var script2=document.createElement("script");script2.src="/frontend/bundle-index.bd2889285f9f5c171f96.js",script2.type="text/javascript",script2.async=true,document.getElementsByTagName("body")[0].appendChild(script2);
